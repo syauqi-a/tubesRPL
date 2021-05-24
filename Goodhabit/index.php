4459521b71e64fceea32671db8f580660765d6a5
Ahmad_Syauqi <kahmadsyauqi@upi.edu>
2021-05-24 11:32:52 +0700

    [dev v.0.9.24.3] perbaikan kecil file index.php

--- Goodhabit/index.php
+++ Goodhabit/index.php
@@ -1,5 +1,12 @@
 <?php
 
-	header("location:page/home.html");
+	if(isset($_COOKIE['id_akun'])){
+		if($_COOKIE['id_akun'] == 1)
+			header("location:admin/home.html");
+		else
+			header("location:home.html");
+	}
+	else
+		header("location:../login.html");
 
 ?>
--- Goodhabit/page/index.php
+++ Goodhabit/page/index.php
@@ -1,5 +1,12 @@
 <?php
 
-	header("location:home.html");
+	if(isset($_COOKIE['id_akun'])){
+		if($_COOKIE['id_akun'] == 1)
+			header("location:../admin/home.html");
+		else
+			header("location:home.html");
+	}
+	else
+		header("location:../login.html");
 
 ?>

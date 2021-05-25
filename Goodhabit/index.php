07a299b9ec708c2c3ec4fefe545b887395865727
Ahmad_Syauqi <kahmadsyauqi@upi.edu>
2021-05-25 09:35:16 +0700

    [dev v.0.9.25.1] perbaikan kecil di Goodhabit/index.php

--- Goodhabit/index.php
+++ Goodhabit/index.php
@@ -4,7 +4,7 @@
 		if($_COOKIE['id_akun'] == 1)
 			header("location:admin/home.html");
 		else
-			header("location:home.html");
+			header("location:page/home.html");
 	}
 	else
 		header("location:../login.html");
